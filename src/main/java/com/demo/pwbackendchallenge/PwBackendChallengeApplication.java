9b8ee0fac446b3f3b09a828061c6f0b1f9c4689f
Moritz Bergemann <moritz.bergemann@gmail.com>
2022-08-24 23:11:02 +0800

    initial commit (based on spring initializr)

---
+++ src/main/java/com/demo/pwbackendchallenge/PwBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pwbackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/pwbackendchallenge/PwBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pwbackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
